34b6939184063dbde4fda89bceed37dc788e50a7
Muhammad Jazzel Mehmood <jazzelmehmood4@gmail.com>
2022-08-23 08:31:55 +0500

    Added auth templates

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.set("view engine", "ejs");
 
 app.use("", require("./routes/home"));
+app.use("", require("./routes/auth"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
